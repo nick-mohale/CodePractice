fd893ac9af16c68fcb8d790783faabc95e169329
Nick Mohale <52918728+nickmohale-collab@users.noreply.github.com>
2023-10-22 15:33:51 +0200

    Constructor Overload

---
+++ C_Sharp_Constructor/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Constructor
+{
+    internal class Person
+    {
+        public int Id;
+        public string Name;
+        public int Age;
+
+        public Person()
+        {
+
+        }
+        public Person( int id)
+        {
+            this.Id = id;
+        }
+        public Person( string name )
+        {
+            this.Name = name;
+        }
+        public Person(int age, string name )
+        {
+            this.Age=age;
+            this.Name=name;
+        }
+    }
+}
