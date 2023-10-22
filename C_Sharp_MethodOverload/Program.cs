e7477d157d626ca27ed69073157fbaa4a9ff6fea
Nick Mohale <52918728+nickmohale-collab@users.noreply.github.com>
2023-10-22 17:26:49 +0200

    Method Overload

---
+++ C_Sharp_MethodOverload/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_MethodOverload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number1");
+            var num1 = Console.ReadLine();
+
+            Console.WriteLine("Enter Number2");
+            var num2 = Console.ReadLine();
+
+            var calculator = new Calculator();
+            calculator.Number1 = int.Parse(num1);
+            calculator.Number2 = int.Parse(num2);
+
+            int sum = calculator.Sum(calculator.Number1, calculator.Number2);
+            int diff = calculator.Sub(calculator.Number1, calculator.Number2);
+            double prod = calculator.prod(calculator.Number1, calculator.Number2);
+
+            Console.WriteLine (calculator.Number1 + " + " + calculator.Number2 + " = " + sum);
+            Console.WriteLine(calculator.Number1 + " - " + calculator.Number2 + " = " + diff);
+            Console.WriteLine(calculator.Number1 + " * " + calculator.Number2 + " = " + prod);
+            Console.ReadKey();
+
+        }
+    }
+}
